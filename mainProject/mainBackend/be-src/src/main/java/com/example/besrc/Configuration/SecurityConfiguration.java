550047022bc66e837d935d928a6f98728c81f0f8
PigCassoKien <kien0610minh@gmail.com>
2025-05-08 01:58:07 +0700

    Update Cors

--- mainProject/mainBackend/be-src/src/main/java/com/example/besrc/Configuration/SecurityConfiguration.java
+++ mainProject/mainBackend/be-src/src/main/java/com/example/besrc/Configuration/SecurityConfiguration.java
@@ -6,6 +6,7 @@ import com.example.besrc.Security.JwtAuthenticationFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -37,6 +38,7 @@ public class SecurityConfiguration {
 
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/swagger-ui.html").permitAll()
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                         .requestMatchers("/api/auth/**").permitAll()
                         .requestMatchers("/api/film/getFilm/**").permitAll()
                         .requestMatchers("/api/film/getFilms").permitAll()
